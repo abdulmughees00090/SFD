91c4d98200598ad35b9c3dabee71492b3ad0e8f8
Abdul Mughees <77141705+abdulmughees00090@users.noreply.github.com>
2025-08-19 21:14:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ if (hamburger) {
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
+    document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navMenu = document.querySelector(".nav-menu");
+
+  if (menuToggle && navMenu) {
+    menuToggle.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+  }
+});
 }
+
